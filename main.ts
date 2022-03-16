066b4ed37f8d63227c07ef39dc8e69931d7a753f
manes08 <101406824+manes08@users.noreply.github.com>
2022-03-16 14:26:14 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,13 @@
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.AB)) {
+    	
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
